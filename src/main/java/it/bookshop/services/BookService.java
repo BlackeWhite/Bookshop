efbbdc907e7448b725439afbc70a033d5c6aac1e
alessandromanilii <alessandromanilii@gmail.com>
2020-12-07 16:18:44 +0100

    implemented BookService and BookServiceDefault

--- src/main/java/it/bookshop/services/BookService.java
+++ src/main/java/it/bookshop/services/BookService.java
@@ -1,5 +1,15 @@
 package it.bookshop.services;
 
-public interface BookService {
+import java.sql.Date;
+import java.util.List;
 
-}
+import it.bookshop.model.entity.Book;
+
+public interface BookService {
+	Book findByIsbn(String isbn);
+	List<Book> findAll();
+	Book create(String isbn, String title, Date publish_date, int num_of_pages, String summary, String cover);
+	Book update(Book book);
+	void delete(Book book);
+	void delete(String isbn);
+};
--- src/main/java/it/bookshop/services/BookServiceDefault.java
+++ src/main/java/it/bookshop/services/BookServiceDefault.java
@@ -1,5 +1,56 @@
 package it.bookshop.services;
 
-public class BookServiceDefault {
+import java.sql.Date;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import it.bookshop.model.dao.AuthorDao;
+import it.bookshop.model.dao.BookDao;
+import it.bookshop.model.entity.Author;
+import it.bookshop.model.entity.Book;
+
+@Transactional
+@Service("bookService")
+public class BookServiceDefault implements BookService {
+
+	private BookDao bookRepository;
+	
+	@Override
+	public 	Book findByIsbn(String isbn) {
+		return bookRepository.findByIsbn(isbn);
+	}
+	
+	@Override
+	public List<Book> findAll() {
+		return bookRepository.findAll();
+	}
+	
+	@Override
+	public Book create(String isbn, String title, Date publish_date, int num_of_pages, String summary, String cover) {
+		return bookRepository.create(isbn, title, publish_date, num_of_pages, summary, cover);
+	}
+	
+	@Override
+	public Book update(Book book) {
+		return bookRepository.update(book);
+	}
+	
+	@Override
+	public void delete(Book book) {
+		bookRepository.delete(book);
+	}
+
+	@Override
+	public void delete(String isbn) {
+		Book b = bookRepository.findByIsbn(isbn);
+		bookRepository.delete(b);
+	}
+
+	@Autowired
+	public void setBookRepository(BookDao bookRepository) {
+		this.bookRepository = bookRepository;
+	}
 }
