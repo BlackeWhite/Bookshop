d265407114cae56dadba2f7762289680afa6197e
ABTCoder <amalbenson98@gmail.com>
2021-01-31 18:21:20 +0100

    Mini modal cart now shows up correctly in account page.

--- src/main/java/it/bookshop/controller/AuthController.java
+++ src/main/java/it/bookshop/controller/AuthController.java
@@ -1,5 +1,6 @@
 package it.bookshop.controller;
 
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -19,6 +20,7 @@ import it.bookshop.model.entity.Genre;
 import it.bookshop.model.entity.PaymentCard;
 import it.bookshop.model.entity.PersonalData;
 import it.bookshop.model.entity.Role;
+import it.bookshop.model.entity.ShoppingCart;
 import it.bookshop.model.entity.User;
 import it.bookshop.services.BookService;
 import it.bookshop.services.UserDetailsServiceDefault;
@@ -29,111 +31,117 @@ public class AuthController {
 
 	@Autowired
 	String appName;
-	
+
 	@Autowired
 	private UserService userService;
-	
+
 	@Autowired
 	private RoleDao roleDao;
-	
+
 	@Autowired
 	private BookService bookService;
-	
-    @GetMapping(value = "/login")
-    public String loginPage(@RequestParam(value = "error", required = false) String error,
-                            Model model) {
-        String errorMessage = null;
-        if(error != null) {
-        	errorMessage = "Username o Password errati !!";
-        }
-        
-        List<Genre> allGenres = this.bookService.getAllGenres();
 
-        model.addAttribute("errorMessage", errorMessage);
-        model.addAttribute("allGenres", allGenres);
-        model.addAttribute("appName", appName);
-        return "login";
-    }
-    
-    @GetMapping(value = "/register")
-    public String registerPage(@RequestParam(value = "error", required = false) String error,
-                            Model model) {
-        String errorMessage = null;
-        Map<String, String> countries = new LinkedHashMap<String, String>();
-        countries.put("Italia", "Italia");
-        countries.put("Germania", "Germania");
-        countries.put("Francia", "Francia");
-        countries.put("Svizzera", "Svizzera");
+	@GetMapping(value = "/login")
+	public String loginPage(@RequestParam(value = "error", required = false) String error, Model model) {
+		String errorMessage = null;
+		if (error != null) {
+			errorMessage = "Username o Password errati !!";
+		}
 
-        model.addAttribute("errorMessage", errorMessage);
-        model.addAttribute("appName", appName);
-        model.addAttribute("countries", countries);
-        model.addAttribute("newUser", new User());
-        
-        List<Genre> allGenres = this.bookService.getAllGenres();
-        model.addAttribute("allGenres", allGenres);
-        
-        return "register";
-    }
-    
-    @PostMapping(value = "/register")
-	public String register(@ModelAttribute("newUser") User user, BindingResult br) {
-    	
-    	//TODO implementare la validazione e il password confirmation
-    	Role user_role = roleDao.findByName("USER");
-    	user.addRole(user_role);
-		this.userService.create(user);
-		
-		return "redirect:/login";
-		
-		// return "redirect:singers/list"; // NB questo non funzionerebbe!
-		
+		List<Genre> allGenres = this.bookService.getAllGenres();
+
+		model.addAttribute("errorMessage", errorMessage);
+		model.addAttribute("allGenres", allGenres);
+		model.addAttribute("appName", appName);
+		return "login";
 	}
-    
-    @GetMapping(value = "/account")
-    public String accountPage(@RequestParam(value = "error", required = false) String error,
-            Model model, Authentication authentication) {
-    	
-    	String principal_name = authentication.getName();
+
+	@GetMapping(value = "/register")
+	public String registerPage(@RequestParam(value = "error", required = false) String error, Model model) {
+		String errorMessage = null;
+		Map<String, String> countries = new LinkedHashMap<String, String>();
+		countries.put("Italia", "Italia");
+		countries.put("Germania", "Germania");
+		countries.put("Francia", "Francia");
+		countries.put("Svizzera", "Svizzera");
+
+		model.addAttribute("errorMessage", errorMessage);
+		model.addAttribute("appName", appName);
+		model.addAttribute("countries", countries);
+		model.addAttribute("newUser", new User());
+
+		List<Genre> allGenres = this.bookService.getAllGenres();
+		model.addAttribute("allGenres", allGenres);
+
+		return "register";
+	}
+
+	@PostMapping(value = "/register")
+	public String register(@ModelAttribute("newUser") User user, BindingResult br) {
+
+		// TODO implementare la validazione e il password confirmation
+		Role user_role = roleDao.findByName("USER");
+		user.addRole(user_role);
+		this.userService.create(user);
+
+		return "redirect:/login";
+
+		// return "redirect:singers/list"; // NB questo non funzionerebbe!
+
+	}
+
+	@GetMapping(value = "/account")
+	public String accountPage(@RequestParam(value = "error", required = false) String error, Model model,
+			Authentication authentication) {
+
+		String principal_name = authentication.getName();
 		User currentUser = userService.findUserByUsername(principal_name);
 		PaymentCard newCard = new PaymentCard();
-    	
-    	List<Genre> allGenres = this.bookService.getAllGenres();
 
-        Map<String, String> countries = new LinkedHashMap<String, String>();
-        countries.put("Italia", "Italia");
-        countries.put("Germania", "Germania");
-        countries.put("Francia", "Francia");
-        countries.put("Svizzera", "Svizzera");
-        
-        Map<String, String> cardTypes = new LinkedHashMap<String, String>();
-        cardTypes.put("Visa", "Visa");
-        cardTypes.put("MasterCard", "MasterCard");
-        cardTypes.put("Bancomat", "Bancomat");
-        cardTypes.put("PostePay", "PostePay");
+		List<Genre> allGenres = this.bookService.getAllGenres();
 
-        model.addAttribute("allGenres", allGenres);
-        model.addAttribute("countries", countries);
-        model.addAttribute("cardTypes", cardTypes);
-        model.addAttribute("currentUser", currentUser);
-        model.addAttribute("newCard", newCard);
-    	return "account";
-    }
-    
-    @PostMapping(value = "/add_payment_card")
-    public String addPaymentCard(@ModelAttribute("newCard") PaymentCard newCard, BindingResult br,
-    		Authentication authentication) {
-    	
-    	String principal_name = authentication.getName();
+		Map<String, String> countries = new LinkedHashMap<String, String>();
+		countries.put("Italia", "Italia");
+		countries.put("Germania", "Germania");
+		countries.put("Francia", "Francia");
+		countries.put("Svizzera", "Svizzera");
+
+		Map<String, String> cardTypes = new LinkedHashMap<String, String>();
+		cardTypes.put("Visa", "Visa");
+		cardTypes.put("MasterCard", "MasterCard");
+		cardTypes.put("Bancomat", "Bancomat");
+		cardTypes.put("PostePay", "PostePay");
+
+		model.addAttribute("allGenres", allGenres);
+		model.addAttribute("countries", countries);
+		model.addAttribute("cardTypes", cardTypes);
+		model.addAttribute("currentUser", currentUser);
+		model.addAttribute("newCard", newCard);
+		
+		// Mini carrello
+		if (authentication != null) {
+			List<ShoppingCart> user_cart = new ArrayList<ShoppingCart>(currentUser.getShoppingCart());
+			model.addAttribute("user_cart", user_cart);
+			model.addAttribute("cartTotalPrice", currentUser.getFormattedCartTotalPrice());
+			model.addAttribute("cartTotalItems", currentUser.getCartTotalItems());
+		}
+		return "account";
+	}
+
+	@PostMapping(value = "/add_payment_card")
+	public String addPaymentCard(@ModelAttribute("newCard") PaymentCard newCard, BindingResult br,
+			Authentication authentication) {
+
+		String principal_name = authentication.getName();
 		User currentUser = userService.findUserByUsername(principal_name);
 		userService.createPaymentCard(newCard, currentUser);
-    	
-    	return "redirect:/account";
-    }
-    
-    @PostMapping(value = "/account_save")
-    public String accountSave(@ModelAttribute("currentUser") User currentUser, BindingResult br) {
-    	userService.update(currentUser);
-    	return "redirect:/account";
-    }
+
+		return "redirect:/account";
+	}
+
+	@PostMapping(value = "/account_save")
+	public String accountSave(@ModelAttribute("currentUser") User currentUser, BindingResult br) {
+		userService.update(currentUser);
+		return "redirect:/account";
+	}
 }
