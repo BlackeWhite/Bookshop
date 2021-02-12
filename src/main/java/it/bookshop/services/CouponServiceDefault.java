a8bc70bd4703a10f14276aa398aeb3212c69e806
alessandromanilii <alessandromanilii@gmail.com>
2021-02-12 10:04:14 +0100

    Small fix.

--- src/main/java/it/bookshop/services/CouponServiceDefault.java
+++ src/main/java/it/bookshop/services/CouponServiceDefault.java
@@ -3,6 +3,7 @@ package it.bookshop.services;
 import java.sql.Date;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,6 +14,7 @@ import it.bookshop.model.entity.Coupon;
 @Service("couponService")
 public class CouponServiceDefault implements CouponService {
 
+	@Autowired
 	private CouponDao couponRepository;
 	
 	@Override
