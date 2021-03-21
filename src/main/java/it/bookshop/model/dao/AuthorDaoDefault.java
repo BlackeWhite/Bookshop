4b5a25cae471128b579529772e5832be35bb800a
agostinodati <agostino.dati@outlook.it>
2021-03-21 19:53:24 +0100

    Add some comments to AuthorDaoDefault

--- src/main/java/it/bookshop/model/dao/AuthorDaoDefault.java
+++ src/main/java/it/bookshop/model/dao/AuthorDaoDefault.java
@@ -15,12 +15,17 @@ public class AuthorDaoDefault extends DefaultDao implements AuthorDao {
 
 	@Override
 	public Author findById(Long id) {
+		/*
+		 * Metodo per la ricerca tramite di un autore.
+		 */
 		return getSession().find(Author.class, id);
 	}
 
 	@Override
 	public List<Book> findBookForAuthor(Author author) {
-		// restituisce una lista di tutti i libri di un particolare autore
+		/*
+		 * Metodo per la ricerca di tutti i libri associati ad un determinato autore
+		 */
 		return this.getSession()
 				.createQuery("select b from Book b join b.authors a WHERE a.id = :author AND b.removed = 0", Book.class)
 				.setParameter("author", author.getId()).getResultList();
@@ -41,6 +46,9 @@ public class AuthorDaoDefault extends DefaultDao implements AuthorDao {
 		;
 		a.setName(name.trim());
 		if (surname.isEmpty() || surname == null || surname == "" || surname == " ") {
+			/*
+			 * Se non c'è il cognome, inserisce un placeholder
+			 */
 			a.setSurname("#SURNAME_PLACEHOLDER");
 		} else {
 			a.setSurname(surname.trim());
@@ -53,12 +61,12 @@ public class AuthorDaoDefault extends DefaultDao implements AuthorDao {
 			a.setPhoto("profile-placeholder.png");
 		try {
 			/*
-			 * Utilizzato nei test
+			 * Restituisce un'eccezione se l'autore è stato già creato
 			 */
 			if (this.getSession()
 					.createQuery("FROM Author a WHERE a.name = :name and a.surname = :surname", Author.class)
 					.setParameter("name", name).setParameter("surname", surname).getSingleResult() != null) {
-				throw new RuntimeException("Already added author");
+				throw new RuntimeException("L'autore è stato già aggiunto.");
 			}
 		} catch (NoResultException e) {
 		}
@@ -68,6 +76,9 @@ public class AuthorDaoDefault extends DefaultDao implements AuthorDao {
 
 	@Override
 	public Author create(String name, String surname) {
+		/*
+		 * Metodo per la creazione di un autore usando solo il Nome e il Cognome
+		 */
 		Author a = new Author();
 		try {
 			/*
@@ -81,18 +92,21 @@ public class AuthorDaoDefault extends DefaultDao implements AuthorDao {
 		;
 		a.setName(name.trim());
 		if (surname.isEmpty() || surname == null || surname == "" || surname == " ") {
+			/*
+			 * Se non c'è il cognome, inserisce un placeholder
+			 */
 			a.setSurname("#SURNAME_PLACEHOLDER");
 		} else {
 			a.setSurname(surname.trim());
 		}
 		try {
 			/*
-			 * Utilizzato nei test
+			 * Restituisce un'eccezione se l'autore è stato già creato
 			 */
 			if (this.getSession()
 					.createQuery("FROM Author a WHERE a.name = :name and a.surname = :surname", Author.class)
 					.setParameter("name", name).setParameter("surname", surname).getSingleResult() != null) {
-				throw new RuntimeException("Already added author");
+				throw new RuntimeException("L'autore è stato già aggiunto.");
 			}
 		} catch (NoResultException e) {
 		}
@@ -109,16 +123,25 @@ public class AuthorDaoDefault extends DefaultDao implements AuthorDao {
 
 	@Override
 	public List<Author> findAll() {
+		/*
+		 * Metodo per la ricerca di tutti gli autori presenti nel DB.
+		 */
 		return getSession().createQuery("FROM Author a", Author.class).getResultList();
 	}
 
 	@Override
 	public void delete(Author author) {
+		/*
+		 * Metodo per la cancellazione di un autore.
+		 */
 		getSession().delete(author);
 	}
 
 	@Override
 	public Author findByNameAndSurname(String Name, String Surname) {
+		/*
+		 * Metodo per la ricerca di un autore a partire dal Nome e dal Cognome
+		 */
 		try {
 			return this.getSession()
 					.createQuery("FROM Author a WHERE a.name = :name and a.surname = :surname", Author.class)
@@ -129,8 +152,9 @@ public class AuthorDaoDefault extends DefaultDao implements AuthorDao {
 	}
 
 	public List<Book> findBookRemovedForAuthor(Author author) {
-		// restituisce una lista di tutti i libri fuori catalogo di un particolare
-		// autore
+		/*
+		 * Metodo per la ricerca dei libri fuori catalogo di un determinato autore.
+		 */
 		return this.getSession()
 				.createQuery("select b from Book b join b.authors a WHERE a.id = :author AND b.removed = 1", Book.class)
 				.setParameter("author", author.getId()).getResultList();
